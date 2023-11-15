e0b1024bece39d4d06b695004cdc2d0cb3620d14
Caleby <caleby117@gmail.com>
2023-11-15 20:36:36 +0800

    First commit with nvim configs

---
+++ nvim/custom/mappings.lua
@@ -0,0 +1,30 @@
+---@type MappingsTable
+local M = {}
+
+M.general = {
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["J"] = { "mzJ`z", "Joins lines and keeps cursor where it originally was"},
+    ["<C-d>"] = { "<C-d>zz", "jump half page down"},
+    ["<C-u>"] = { "<C-u>zz", "jump half page up"},
+    ["n"] = { "nzzzv", "find string while keeping cursor in middle"},
+    ["N"] = { "Nzzzv", "find string while keeping cursor in middle"},
+    ["<leader>d"] = {"\"_d", "preserve the yanked content"},
+    ["Q"] = {"<nop>", "do not press Q"},
+  },
+  v = {
+    [">"] = { ">gv", "indent"},
+    ["J"] = { ":m '>+1<CR>gv=gv", "move chunk up"},
+    ["K"] = { ":m '<-2<CR>gv=gv", "move chunk down"},
+    ["<leader>d"] = {"\"_d", "preserve the yanked content"},
+  },
+
+  x = {
+    ["<leader>p"] = {"\"_dP", "preserve the yanked content"},
+
+  }
+}
+
+-- more keybinds!
+
+return M
