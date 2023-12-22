9b379833388af70136b01f5fe40ecfba3b59a301
Caleb Yoong <caleb.yoong216@gmail.com>
2023-12-22 09:51:49 +0800

    pre christmas config

--- nvim/custom/configs/null-ls.lua
+++ nvim/custom/configs/null-ls.lua
@@ -1,26 +1,29 @@
-local null_ls = require("null-ls")
+local null_ls = require "null-ls"
 
 local b = null_ls.builtins
 
 local sources = {
 
-	-- webdev stuff
-	b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-	b.formatting.prettier.with({ filetypes = { "html", "markdown", "css" } }), -- so prettier works only on these filetypes
+  -- webdev stuff
+  b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
+  b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
 
-	-- Lua
-	b.formatting.stylua,
+  -- Lua
+  b.formatting.stylua,
 
-	-- cpp
-	b.formatting.clang_format,
+  -- cpp
+  b.formatting.clang_format,
 
-	-- Python
-	-- b.diagnostics.ruff,
-	b.formatting.black,
+  -- Python
+  -- b.diagnostics.ruff,
+  b.formatting.black,
+
+  -- shellcheck
+  b.diagnostics.shellcheck,
 }
 
-null_ls.setup({
-	sources = sources,
-	update_in_insert = true,
-	root_dir = require("null-ls.utils").root_pattern(".null-ls-root", "Makefile", ".git", ".ccls"),
-})
+null_ls.setup {
+  sources = sources,
+  update_in_insert = true,
+  root_dir = require("null-ls.utils").root_pattern(".null-ls-root", "Makefile", ".git", ".ccls"),
+}
--- nvim/custom/mappings.lua
+++ nvim/custom/mappings.lua
@@ -2,30 +2,30 @@
 local M = {}
 
 M.general = {
-	n = {
-		[";"] = { ":", "enter command mode", opts = { nowait = true } },
-		["J"] = { "mzJ`z", "Joins lines and keeps cursor where it originally was" },
-		["<C-d>"] = { "<C-d>zz", "jump half page down" },
-		["<C-u>"] = { "<C-u>zz", "jump half page up" },
-		["n"] = { "nzzzv", "find string while keeping cursor in middle" },
-		["N"] = { "Nzzzv", "find string while keeping cursor in middle" },
-		["<leader>d"] = { '"_d', "preserve the yanked content" },
-		["Q"] = { "<nop>", "do not press Q" },
-		["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
-		["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
-		["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
-		["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
-	},
-	v = {
-		[">"] = { ">gv", "indent" },
-		["J"] = { ":m '>+1<CR>gv=gv", "move chunk up" },
-		["K"] = { ":m '<-2<CR>gv=gv", "move chunk down" },
-		["<leader>d"] = { '"_d', "preserve the yanked content" },
-	},
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["J"] = { "mzJ`z", "Joins lines and keeps cursor where it originally was" },
+    ["<C-d>"] = { "<C-d>zz", "jump half page down" },
+    ["<C-u>"] = { "<C-u>zz", "jump half page up" },
+    ["n"] = { "nzzzv", "find string while keeping cursor in middle" },
+    ["N"] = { "Nzzzv", "find string while keeping cursor in middle" },
+    ["<leader>d"] = { '"_d', "preserve the yanked content" },
+    ["Q"] = { "<nop>", "do not press Q" },
+    ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
+    ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
+    ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
+    ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
+  },
+  v = {
+    [">"] = { ">gv", "indent" },
+    ["J"] = { ":m '>+1<CR>gv=gv", "move chunk up" },
+    ["K"] = { ":m '<-2<CR>gv=gv", "move chunk down" },
+    ["<leader>d"] = { '"_d', "preserve the yanked content" },
+  },
 
-	x = {
-		["<leader>p"] = { '"_dP', "preserve the yanked content" },
-	},
+  x = {
+    ["<leader>p"] = { '"_dP', "preserve the yanked content" },
+  },
 }
 
 -- more keybinds!
